01d48fa70145a97d6adb0b60186eae4f210436ac
bigred42 <raynewstead@gmail.com>
2014-06-08 20:51:18 +1200

    Extended Python Script
    Script can now take 'open' or 'write' along with a file name of a zip,
    and, in the case of 'write' extra file names to write to the zip.
    Currently can not create empty folders in the zip. Open will just open
    and read the contents of the zip, but will not display the results.

--- bin/win32/ArchiverMain.py
+++ bin/win32/ArchiverMain.py
@@ -1,11 +1,13 @@
 import sys
 import os
+import zipfile
 
 def archiverMain():
     #Definitions
     i = 0
     fileName = ""
     mode = ""
+    contents = []
 
     #Read command line parameters
     for arg in sys.argv:
@@ -13,38 +15,66 @@ def archiverMain():
             mode = arg
         elif i==2:
             fileName = arg
-            break
+            if mode != "write":
+                break
+        elif i>2:
+            contents.append({'name':arg, 'basename':os.path.basename(arg)})
         i=i+1
 
     #Check for valid input
     if mode == "open" and fileName != "":
         #Check valid input file
-        if os.path.isfile(fileName):
-            print("Valid file name supplied")
-        else:
+        if not(os.path.isfile(fileName)):
             print("Invalid file name supplied")
+        else:
+            archive = Archive(fileName)
+            archive.open()
     elif mode == "write" and fileName != "":
         #Create output file to validate name
         try:
-            outFile = open(fileName, "w+")
-            #TEMPORARY DELETE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<-------I WANT ATTENTION!
-            outFile.close()
-            os.remove(fileName)
-            #TEMPORARY DELETE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<-------I WANT ATTENTION!
-        except FileNotFoundError:
-            print("\"" + fileName + "\" is an invalid file name. Could not write to file.")
+            archive = Archive(fileName)
+            archive.write(contents)
+        except Exception:
+            print("There was an error writing to file. Please ensure files are in the same folder as the .py, and the filenames are valid.")
     else:
         print("Incorrect command line parameters supplied")
 
 class Archive:
-    def __init__(self, fileName):
+    #GENERAL (EXTENSION INDEPENDANT) METHODS
+    def __init__(self, archive):
         #Constructor
-        self.fileName = fileName
-        fileExtension = self.fetchExtension(fileName)
+        self.fileName = archive
+        self.fileExtension = self.fetchExtension(archive)
 
     def fetchExtension(self, fileName):
         #Fetches file extension to define archive type
         return self.fileName[self.fileName.rfind('.')+1:]
+
+    def open(self):
+        if self.fileExtension == "zip":
+            self.openZip()
+
+    def write(self, contents):
+        if self.fileExtension == "zip":
+            self.writeZip(contents)
+
+    #EXTENSION SPECIFIC METHODS
+    def openZip(self):
+        #open the zip
+        self.a = zipfile.ZipFile(self.fileName, "r")
+        #fetch the files/folders
+        self.aNameList = self.a.namelist()
+        #close the file
+        self.a.close()
+
+    def writeZip(self, contents):
+        #open the zip for writing
+        self.a = zipfile.ZipFile(self.fileName, "w")
+        #write files
+        for data in contents:
+            self.a.write(data['basename'], data['name'], zipfile.ZIP_DEFLATED)
+        #close the file
+        self.a.close()
     
 #Start execution
 archiverMain()
