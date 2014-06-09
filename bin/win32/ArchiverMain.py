170155cdf56caed8cf676a2ba8e45be7ee1a06d0
bigred42 <raynewstead@gmail.com>
2014-06-09 16:31:58 +1200

    Python TAR, GZ and BZ2 support
    Added support for TAR, TAR.GZ, TGZ, and TAR.BZ2. Lone BZ2 and GZ not yet
    supported.

--- bin/win32/ArchiverMain.py
+++ bin/win32/ArchiverMain.py
@@ -1,7 +1,8 @@
 import sys
 import os
-import zipfile
-import rarfile
+import zipfile #zip file
+import rarfile #rar file (open only)
+import tarfile #bz2 and gzip files
 
 def archiverMain():
     #Definitions
@@ -35,8 +36,8 @@ def archiverMain():
         try:
             archive = Archive(fileName)
             archive.write(contents)
-        except Exception:
-            print("There was an error writing to file. Please ensure files are in the same folder as the .py, and the filenames are valid.")
+        except Exception as e:
+            print("There was an error writing to file. Please ensure files are in the same folder as the .py, and the filenames are valid.\n" + e)
     else:
         print("Incorrect command line parameters supplied")
 
@@ -49,24 +50,36 @@ class Archive:
 
     def fetchExtension(self, fileName):
         #Fetches file extension to define archive type
-        return self.fileName[self.fileName.rfind('.')+1:]
+        if self.fileName[self.fileName.rfind('.')+1:] == "gz" or self.fileName[self.fileName.rfind('.')+1:] == "bz2":
+            extension = self.fileName[self.fileName[:self.fileName.rfind('.')].rfind('.')+1:]
+            if extension == "tar.gz" or extension == "tar.bz2":
+                return extension
+            else:
+                return self.fileName[self.fileName.rfind('.')+1:]
+        else:
+            return self.fileName[self.fileName.rfind('.')+1:]
 
     def open(self):
-        if self.fileExtension == "zip":
+        if self.fileExtension == "zip" and zipfile.is_zipfile(self.fileName):
             self.openZip()
-        elif self.fileExtension == "rar":
+        elif self.fileExtension == "rar" and rarfile.is_rarfile(self.fileName):
             self.openRar()
+        elif (self.fileExtension == "tar.gz" or self.fileExtension == "tar.bz2" or self.fileExtension == "tar" or self.fileExtension == "tgz") and tarfile.is_tarfile(self.fileName):
+            self.openTar()
 
     def write(self, contents):
         if self.fileExtension == "zip":
             self.writeZip(contents)
-
+        elif self.fileExtension == "tar.gz" or self.fileExtension == "tar.bz2" or self.fileExtension == "tar" or self.fileExtension == "tgz":
+            self.writeTar(contents)
+            
     #EXTENSION SPECIFIC METHODS
     def openZip(self):
         #open the zip
         self.a = zipfile.ZipFile(self.fileName, "r")
         #fetch the files/folders
         self.aNameList = self.a.namelist()
+        print("Success")
         #close the file
         self.a.close()
 
@@ -84,6 +97,35 @@ class Archive:
         self.a = rarfile.RarFile(self.fileName, "r")
         #fetch the files/folders
         self.aNameList = self.a.namelist()
+        print("Success")
+        #close the file
+        self.a.close()
+
+    def openTar(self):
+        #open the tar
+        if self.fileExtension == "tar":
+            self.a = tarfile.open(self.fileName, "r:")
+        elif self.fileExtension == "tgz" or self.fileExtension == "tar.gz":
+            self.a = tarfile.open(self.fileName, "r:gz")
+        elif self.fileExtension == "tar.bz2":
+            self.a = tarfile.open(self.fileName, "r:bz2")
+        #fetch the files/folders
+        self.aNameList = self.a.getnames()
+        print("Success")
+        #close the file
+        self.a.close()
+
+    def writeTar(self, contents):
+        #open the tar for writing
+        if self.fileExtension == "tar.gz" or self.fileExtension == "tgz":
+            self.a = tarfile.open(self.fileName, "w:gz")
+        elif self.fileExtension == "tar.bz2":
+            self.a = tarfile.open(self.fileName, "w:bz2")
+        else:
+            self.a = tarfile.open(self.fileName, "w")
+        #write files
+        for data in contents:
+            self.a.add(data['basename'], data['name'])
         #close the file
         self.a.close()
     
