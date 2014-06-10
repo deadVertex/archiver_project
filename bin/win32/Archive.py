29d94b5306c4fddbedc1bc92b725fdb2a1b81fb1
bigred42 <raynewstead@gmail.com>
2014-06-10 17:25:22 +1200

    Extract Command, Various Fixes, Minor Cleaning
    open command changed to read for clarity; about command added and
    implemented; extract command added; all formats can now extract all
    their files to a specific folder via the extract command, use about
    command to see more information; various comments clarified and all
    comments placed in a more similar position; invalid file error message
    clarified to file not found; Removed a vague write error, will need to
    add more specific write error try excepts later during testing.

--- bin/win32/Archive.py
+++ bin/win32/Archive.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 import zipfile #zip files
 import rarfile #rar files (open only)
 import tarfile #tar, tar.bz2 and tar.gz/tgz files
@@ -8,13 +9,14 @@ import gzip #gz files
 
 class Archive:
     #GENERAL (EXTENSION INDEPENDANT) METHODS
+    #Constructor
     def __init__(self, archive):
-        #Constructor
         self.fileName = archive
         self.fileExtension = self.fetchExtension(archive)
+        self.extractMode = False
 
+    #Fetches file extension to define archive type
     def fetchExtension(self, fileName):
-        #Fetches file extension to define archive type
         if self.fileName[self.fileName.rfind('.')+1:] == "gz" or self.fileName[self.fileName.rfind('.')+1:] == "bz2":
             extension = self.fileName[self.fileName[:self.fileName.rfind('.')].rfind('.')+1:]
             if extension == "tar.gz" or extension == "tar.bz2":
@@ -24,22 +26,47 @@ class Archive:
         else:
             return self.fileName[self.fileName.rfind('.')+1:]
 
-    def open(self):
+    #Reads the archive for its file(s), checking to see if the archive has to be extracted
+    def open(self, loc=""):
         if self.fileExtension == "zip" and zipfile.is_zipfile(self.fileName):
-            self.openZip()
+            if self.extractMode:
+                self.openZip(loc)
+            else:
+                self.openZip()
         elif self.fileExtension == "rar" and rarfile.is_rarfile(self.fileName):
-            self.openRar()
+            if self.extractMode:
+                self.openRar(loc)
+            else:
+                self.openRar()
         elif (self.fileExtension == "tar.gz" or self.fileExtension == "tar.bz2" or self.fileExtension == "tar" or self.fileExtension == "tgz") and tarfile.is_tarfile(self.fileName):
-            self.openTar()
+            if self.extractMode:
+                self.openTar(loc)
+            else:
+                self.openTar()
         elif self.fileExtension == "lz" or self.fileExtension == "xz":
-            self.openLzma()
+            if self.extractMode:
+                self.openLzma(loc)
+            else:
+                self.openLzma()
         elif self.fileExtension == "7z":
-            self.open7z()
+            if self.extractMode:
+                self.open7z(loc)
+            else:
+                self.open7z()
         elif self.fileExtension == "bz2":
-            self.openBz2()
+            if self.extractMode:
+                self.openBz2(loc)
+            else:
+                self.openBz2()
         elif self.fileExtension == "gz":
-            self.openGz()
+            if self.extractMode:
+                self.openGz(loc)
+            else:
+                self.openGz()
+        else:
+            print("Invalid/No file extension found!")
 
+    #Writes the contents of 'contents' to the archive
     def write(self, contents):
         if self.fileExtension == "zip":
             self.writeZip(contents)
@@ -47,14 +74,32 @@ class Archive:
             self.writeTar(contents)
         elif self.fileExtension == "7z":
             self.write7z(contents)
+        elif self.fileExtension == "bz2":
+            self.writeBz2(contents)
+        elif self.fileExtension == "gz":
+            self.writeGz(contents)
+        else:
+            print("Invalid/No file extension found!")
+
+    def extract(self, loc):
+        #remove the filename from location for certain archive types to use
+        loc = os.path.split(loc)
+        #Find the correct archive extension, and extract
+        self.extractMode = True
+        self.open(loc)
             
     #EXTENSION SPECIFIC METHODS
-    def openZip(self):
+    def openZip(self, loc=""):
         #open the zip
         self.a = zipfile.ZipFile(self.fileName, "r")
         #fetch the files/folders
         self.aNameList = self.a.namelist()
-        print("Success")
+        if self.extractMode:
+            self.a.extractall(loc[0])
+            print("All files in " + self.fileName + " written to " + loc[0] + "/.")
+        else:
+            for each in self.aNameList:
+                print(each)
         #close the file
         self.a.close()
 
@@ -67,16 +112,21 @@ class Archive:
         #close the file
         self.a.close()
 
-    def openRar(self):
+    def openRar(self, loc=""):
         #open the zip
         self.a = rarfile.RarFile(self.fileName, "r")
         #fetch the files/folders
         self.aNameList = self.a.namelist()
-        print("Success")
+        if self.extractMode:
+            self.a.extractall(loc[0])
+            print("All files in " + self.fileName + " written to " + loc[0] + "/.")
+        else:
+            for each in self.aNameList:
+                print(each)
         #close the file
         self.a.close()
 
-    def openTar(self):
+    def openTar(self, loc=""):
         #open the tar
         if self.fileExtension == "tar":
             self.a = tarfile.open(self.fileName, "r:")
@@ -86,7 +136,12 @@ class Archive:
             self.a = tarfile.open(self.fileName, "r:bz2")
         #fetch the files/folders
         self.aNameList = self.a.getnames()
-        print("Success")
+        if self.extractMode:
+            self.a.extractall(loc[0])
+            print("All files in " + self.fileName + " written to " + loc[0] + "/.")
+        else:
+            for each in self.aNameList:
+                print(each)
         #close the file
         self.a.close()
 
@@ -104,11 +159,22 @@ class Archive:
         #close the file
         self.a.close()
 
-    def openLzma(self):
+    def openLzma(self, loc=""):
         #open the lzma
         self.a = lzma.open(self.fileName, "r")
         #read contents
-        file_content = self.a.read()
+        self.aNameList = self.fileName[self.fileName.rfind('/')+1:self.fileName.rfind('.')]
+        if self.extractMode:
+            if loc[1] != "":
+                b = open(loc[0] + "/" + loc[1], "wb")
+                b.write(self.a.read())
+                b.close()
+                print("File " + loc[1] + " written to " + loc[0] + "/.")
+            else:
+                b = open(loc[0] + "/" + self.fileName[self.fileName.rfind('/')+1:self.fileName.rfind('.')], "wb")
+                b.write(self.a.read())
+                b.close()
+                print("File " + self.fileName[self.fileName.rfind('/')+1:self.fileName.rfind('.')] + " written to " + loc[0] + "/.")
         print("Success")
         #close the file
         self.a.close()
@@ -121,13 +187,16 @@ class Archive:
         #close the file
         self.a.close()
 
-    def open7z(self):
+    def open7z(self, loc=""):
         #open the 7z
-        self.a = subprocess.call("7za.exe l " + self.fileName)
+        if self.extractMode:
+            print("7za.exe e " + self.fileName + " -o" + loc[0] + "/ -y")
+            self.a = subprocess.call("7za.exe e " + self.fileName + " -o" + loc[0] + "/ -y")
+        else:
+            self.a = subprocess.call("7za.exe l " + self.fileName)
         #This line will catch the 7za.exe's output and return it as a byte array
         #self.a = subprocess.check_output("7za.exe l -slt " + self.fileName)#.decode('utf-8')#Converts byte array to string
         #print(self.a)
-        print("Success")
 
     def write7z(self, contents):
         #give the commands to 7za.exe for compressing
@@ -135,12 +204,22 @@ class Archive:
             self.a = subprocess.check_output("7za.exe a " + self.fileName + " " + each['name'])
         print("Success")
 
-    def openBz2(self):
+    def openBz2(self, loc=""):
         #open the bz2
         self.a = bz2.open(self.fileName, "r")
         #read the data
-        self.aNameList = self.a.read()
-        print("Success")
+        self.aNameList = [self.fileName[self.fileName.rfind('/')+1:self.fileName.rfind('.')]]
+        if self.extractMode:
+            if loc[1] != "":
+                b = open(loc[0] + "/" + loc[1], "wb")
+                b.write(self.a.read())
+                b.close()
+                print("File " + loc[1] + " written to " + loc[0] + "/.")
+            else:
+                b = open(loc[0] + "/" + self.fileName[self.fileName.rfind('/')+1:self.fileName.rfind('.')], "wb")
+                b.write(self.a.read())
+                b.close()
+                print("File " + self.fileName[self.fileName.rfind('/')+1:self.fileName.rfind('.')] + " written to " + loc[0] + "/.")
         #close the file
         self.a.close()
 
@@ -148,16 +227,26 @@ class Archive:
         #open the bz2 for writing
         self.a = bz2.open(self.fileName, "w")
         #write data
-        self.a.write(contents[0]["basename"])
+        self.a.writelines(open(contents[0]["basename"], 'rb'))
         #close the file
         self.a.close()
 
-    def openGz(self):
+    def openGz(self, loc=""):
         #open the gz
         self.a = gzip.open(self.fileName, "r")
         #read data
-        self.aNameList = self.a.read()
-        print("Success")
+        self.aNameList = [self.fileName[self.fileName.rfind('/')+1:self.fileName.rfind('.')]]
+        if self.extractMode:
+            if loc[1] != "":
+                b = open(loc[0] + "/" + loc[1], "wb")
+                b.write(self.a.read())
+                b.close()
+                print("File " + loc[1] + " written to " + loc[0] + "/.")
+            else:
+                b = open(loc[0] + "/" + self.fileName[self.fileName.rfind('/')+1:self.fileName.rfind('.')], "wb")
+                b.write(self.a.read())
+                b.close()
+                print("File " + self.fileName[self.fileName.rfind('/')+1:self.fileName.rfind('.')] + " written to " + loc[0] + "/.")
         #close the file
         self.a.close()
 
@@ -165,6 +254,6 @@ class Archive:
         #open the gz for writing
         self.a = gzip.open(self.fileName, "w")
         #write data
-        self.a.write(contents[0]["basename"])
+        self.a.writelines(open(contents[0]["basename"], 'rb'))
         #close the file
         self.a.close()
--- bin/win32/ArchiverMain.py
+++ bin/win32/ArchiverMain.py
@@ -9,33 +9,64 @@ def archiverMain():
     mode = ""
     contents = []
 
+    #Aesthetic spacing
+    print()
+
     #Read command line parameters
     for arg in sys.argv:
         if i==1:
             mode = arg
         elif i==2:
             fileName = arg
-            if mode != "write":
-                break
         elif i>2:
             contents.append({'name':arg, 'basename':os.path.basename(arg)})
         i=i+1
 
+    if not(os.path.isfile(fileName)) and (mode != "write" and mode != "about"):
+        print("Supplied file does not exist")
+        sys.exit(0)
+    else:
+        archive = Archive.Archive(fileName)
+
     #Check for valid input
-    if mode == "open" and fileName != "":
-        #Check valid input file
-        if not(os.path.isfile(fileName)):
-            print("Invalid file name supplied")
-        else:
-            archive = Archive.Archive(fileName)
-            archive.open()
+    if mode == "read" and fileName != "":
+        #Check valid input file then open the archive
+        archive.open()
+        print(archive.aNameList)
     elif mode == "write" and fileName != "":
-        #Create output file to validate name
-        try:
-            archive = Archive.Archive(fileName)
-            archive.write(contents)
-        except Exception as e:
-            print("There was an error writing to file. Please ensure files are in the same folder as the .py, and the filenames are valid.\n" + e)
+        #write to archive
+        archive.write(contents)
+    elif mode == "extract":
+        #Extract the contents of the archive to a listed folder
+        if contents:
+            archive.extract(contents[0]['name'])
+        else:
+            print("No destination path supplied! Use '/' to specify same directory as script")
+    elif mode == "about":
+        #About/Help prompt
+        print()
+        print("--An Archiver Project--")
+        print()
+        print("File Formats Supported:")
+        print("zip, rar(read only), tar, tar.gz, tar.bz2, gz, bz2, lz, xz, 7z")
+        print()
+        print("--Commands--")
+        print()
+        print("read\tReads the files contained within an archive")
+        print("\tSyntax: open archive_name")
+        print()
+        print("write\tClears and writes a file(s) to an archive")
+        print("\tSyntax: write archive_name [files_to_be_compressed..]")
+        print()
+        print("extract\tExtracts the archive to a listed folder (\"/\" = same folder)")
+        print("\tAlways overwrites files at destination")
+        print("\tThis will ignore file names, except in the case of")
+        print("\t.gz, .bz2 (but not the tar variants), .lz, or .xz")
+        print("\tSyntax: extract archive_name destination_path")
+        print()
+        print("about\tDisplays this help and information utility")
+        print("\tSyntax: about")
+        print()
     else:
         print("Incorrect command line parameters supplied")
     
